2f1b7ba6ab283837c11d01030f7d1dc60b9f3918
LauraWebdev <hello@laura.media>
2024-02-09 12:27:54 +0100

    Switch to Hash History

--- KHID.UI/UserInterface/src/router.js
+++ KHID.UI/UserInterface/src/router.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {createRouter, createWebHashHistory} from 'vue-router';
 import ViewIndex from '@/views/ViewIndex.vue';
 import ViewSettings from '@/views/ViewSettings.vue';
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
